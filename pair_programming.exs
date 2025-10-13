141631b9b895559d227d6618b2dc6e1c09e284b4
Joel Koch <joel@bitcrowd.net>
2025-10-13 14:31:13 +0200

    mix format

--- pair_programming.exs
+++ pair_programming.exs
@@ -1,7 +1,7 @@
 Mix.install([
   {:bumblebee, path: "../bumblebee_bitcrowd"},
   {:nx, "~> 0.10.0", override: true},
-  {:emlx, github: "elixir-nx/emlx"},
+  {:emlx, github: "elixir-nx/emlx"}
 ])
 
 Nx.global_default_backend({EMLX.Backend, device: :gpu})
@@ -11,20 +11,21 @@ repo = {:hf, "HuggingFaceTB/SmolLM2-135M-Instruct"}
 {:ok, generation_config} = Bumblebee.load_generation_config(repo)
 
 sequence_length = 512
-prompt = """
-  Give me 10 random, single digit numbers in an array.
-  Valid examples are:
 
-  [1]
-  [4,7]
-  [2,4,1]
-  [4,5,3,6]
-  [1,7,8,0]
-  [9,4,7,3,5,2]
-  [8,2,3,8,6,4,8]
-  [3,5,9]
-  [8,9,6,7]
-  """
+prompt = """
+Give me 10 random, single digit numbers in an array.
+Valid examples are:
+
+[1]
+[4,7]
+[2,4,1]
+[4,5,3,6]
+[1,7,8,0]
+[9,4,7,3,5,2]
+[8,2,3,8,6,4,8]
+[3,5,9]
+[8,9,6,7]
+"""
 
 generation_config =
   Bumblebee.configure(generation_config,
@@ -33,15 +34,12 @@ generation_config =
     strategy: %{type: :multinomial_sampling, top_p: 0.6}
   )
 
-
 serving =
   Bumblebee.Text.generation(model_info, tokenizer, generation_config,
     compile: [batch_size: 1, sequence_length: sequence_length],
     stream: false,
     defn_options: [compiler: Nx.Defn.Evaluator]
   )
-
-
 
 {:ok, _pid} =
   Supervisor.start_link([{Nx.Serving, name: Serving, serving: serving}], strategy: :one_for_one)
