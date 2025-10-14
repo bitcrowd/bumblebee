8e1f7e3de1c5fcd77bed1d941e9316bc4ea5bc6a
Joel Koch <joel@bitcrowd.net>
2025-10-14 15:18:25 +0200

    wip 2

--- pair_programming.exs
+++ pair_programming.exs
@@ -34,43 +34,64 @@ states_to_num = %{
   in_number: 2,
   in_addition: 3,
   in_string: 4,
-  ending: 5
+  end_of_string: 5,
+  ending: 6
 }
 
 # ------------------------------------- above chars ------------------------------ #
 # ------------------------------------- below tokens ------------------------------ #
 
-end_of_sequence_token_ids = [Bumblebee.Tokenizer.special_token_id(tokenizer, :eos)]
+array_start_token_id = Bumblebee.Tokenizer.token_to_id(tokenizer, array_start_token))
+array_end_token_id = Bumblebee.Tokenizer.token_to_id(tokenizer, array_end_token))
+addition_token_id = Bumblebee.Tokenizer.token_to_id(tokenizer, array_addition_token)
+string_token_id = Bumblebee.Tokenizer.token_to_id(tokenizer, string_token)
+end_of_sequence_token_id = Bumblebee.Tokenizer.special_token_id(tokenizer, :eos)
+
 special_tokens_ids = for token_id <- 0..17, do: token_id
-
-array_start_token_ids = Enum.map(array_start_token, &Bumblebee.Tokenizer.token_to_id(tokenizer, &1))
-array_end_token_ids = Enum.map(array_end_token, &Bumblebee.Tokenizer.token_to_id(tokenizer, &1))
-addition_token_ids = Enum.map(array_addition_token, &Bumblebee.Tokenizer.token_to_id(tokenizer, &1))
-string_token_ids = Enum.map(string_token, &Bumblebee.Tokenizer.token_to_id(tokenizer, &1))
-
 number_tokens_ids = Enum.map(numbers, &Bumblebee.Tokenizer.token_to_id(tokenizer, &1))
 vocabulary_token_ids = for token_id <- 0..model_info.vocabulary_size, do: token_id
-forbidden_string_tokens_ids = string_token_ids -- special_tokens_ids --
-string_token_ids = vocabulary_token_ids -- forbidden_string_tokens_ids # including ", which ends the string
+
+string_token_ids = vocabulary_token_ids -- [string_token_id] -- special_tokens_ids 
 
 ## transitions
 transitions = %{
-  starting: start_token_ids,
-  in_array: number_tokens_ids ++ end_token_ids ++ start_string_token_ids, # todo start string token
-  in_number: number_tokens_ids ++ addition_token_ids ++ end_token_ids,
-  in_addition: number_tokens_ids,
-  in_string: string_token_ids -- forbidden_strin_tokens_ids,
-  ending: end_of_sequence_token_ids
+  starting: [array_start_token_id],
+  in_array: number_tokens_ids ++ [array_end_token_id, string_token_id], # todo start string token
+  in_number: number_tokens_ids ++ [addition_token_id, end_token_id],
+  in_addition: number_tokens_ids ++ [string_token_id],
+  in_string: string_token_ids ++ [string_token_id],
+  end_of_string: [addition_token_id, array_end_token_id],
+  ending: [end_of_sequence_token_id]
 }
+
+## sequence : 75, 33, 34, ...
+
+# State             0   1  
+# chosen Token id  75  18  
+# new state         1   3  
+
+## tensor
+# State/token ids -> new state
+## State / Token ids   0  1  2  ... 18 ... 33 ... 75  76       
+## starting (0)       -1 -1 -1      -1     -1      1
+## in_array (1)                      4      2          6 
+## in_number (2)
+## in_addition (3)
+## in_string (4)
+## end_of_string (5)
+## ending (6)
+
 
 ## states
 states =
   %{
     starting: [],
-    in_array: Enum.map(start_token, &Bumblebee.Tokenizer.token_to_id(tokenizer, &1)),
-    in_number: Enum.map(numbers, &Bumblebee.Tokenizer.token_to_id(tokenizer, &1)),
-    in_addition: Enum.map(addition_token, &Bumblebee.Tokenizer.token_to_id(tokenizer, &1)),
-    ending: Enum.map(end_token, &Bumblebee.Tokenizer.token_to_id(tokenizer, &1))
+    in_array: [array_start_token_id],
+    in_number: number_token_ids,
+    in_addition: [addition_token_id],
+    in_string: [string_token_id],
+    end_of_string: [string_token_id],
+    ending: [array_end_token_id]
   }
   |> Enum.flat_map(fn {state, tensor_ids} ->
     for tensor_id <- tensor_ids do
